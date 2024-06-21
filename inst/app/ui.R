ee181e5efabf4c8c22e5d983df8894f081c7192e
Jefferson Ndeke <musumbijefferson@gmail.com>
2024-06-21 22:26:20 +0300

    Apply stashed changes after pull

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -15,26 +15,6 @@ ui <- argonDash::argonDashPage(
     ) 
   ),
   argonDash::argonDashBody(
-    argonR::argonCard(
-      title = "Upload PDF",
-      shadow = TRUE,
-      border_level = 5,
-      icon = icon("upload"),
-      status = "default",
-      width = 4,
-      shinyjs::useShinyjs(),
-      includeCSS(
-        path = "www/css/styles.css"
-      ),
-      includeScript(
-        path = "www/js/script.js"
-      ),
-      div(
-        fileInput("pdfFile", "Select a PDF", accept = c(".pdf")),
-        actionButton("upload_btn", "Publish PDF")
-      )
-    ),
-    div(
       div(
         shinybusy::add_busy_spinner(
           spin = "fading-circle",
@@ -42,38 +22,6 @@ ui <- argonDash::argonDashPage(
           margins = c("30%", "50%")
         )
       ),
-      uiOutput("published_pdfs", class = "d-flex flex-wrap"),
-      argonR::argonModal(
-        width = 12,
-        id = "modal",
-        title = "Selected Content",
-        status = "secondary",
-        gradient = TRUE,
-        div(
-          id = "modal-content",
-          class = "hover-content", # Apply the hover class to the modal content
-          style = "position: relative;", # Ensure the modal content is relatively positioned
-          div(
-            id = "hover-div",
-            class = "bg-translucent-default rounded pt-3 pb-3",
-            actionButton("prev_btn", "", icon = icon("arrow-left"), class = "bg-gradient-gray") |> remove_btn_default(),
-            actionButton("full_screen_btn", "", icon = icon("expand"), class = "bg-gradient-gray") |> remove_btn_default(),
-            actionButton("next_btn", "", icon = icon("arrow-right"), class = "bg-gradient-gray") |> remove_btn_default(),
-            uiOutput("progress_bar")
-          ),
-          imageOutput("pdf_images", height = "auto", width = "100%")
-        )
-      ),
-      # Hidden button to trigger modal display
-      shinyjs::hidden(
-        argonR::argonButton(
-          name = "Show Modal",
-          status = "primary",
-          icon = NULL,
-          size = "sm",
-          modal_id = "modal",
-        )
-      )
-    )
-  )
+      uiOutput("body_content")
+)
 )
